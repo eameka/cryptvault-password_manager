a3f38894d09fe89720f16f76cbdc3d367458fd5f
Majestyjnr <aidoojuniorsolomon@gmail.com>
2023-08-07 08:52:45 +0000

    Revamped Add Credential Alert Dialog

--- lib/CryptVault.dart
+++ lib/CryptVault.dart
@@ -1,27 +1,29 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-
 class CryptVault extends StatelessWidget {
-   CryptVault({Key? key, required this.accountController, required this.passController}) :super(key: key);
+  CryptVault(
+      {Key? key, required this.accountController, required this.passController})
+      : super(key: key);
 
-
-   String accountController;
-   String passController;
+  String accountController;
+  String passController;
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white60,
-        appBar: AppBar(
-          title: const Text('CryptVault'),
-          leading: IconButton(onPressed: (){
+      appBar: AppBar(
+        title: const Text('CryptVault'),
+        leading: IconButton(
+          onPressed: () {
             Navigator.pop(context);
-          },icon: const Icon(Icons.arrow_back_ios_new),),
-          backgroundColor: Colors.blueAccent,
+          },
+          icon: const Icon(Icons.arrow_back_ios_new),
         ),
-
+        backgroundColor: Colors.blueAccent,
+      ),
       body: Container(
         padding: const EdgeInsets.all(20.0),
         child: ListView(
@@ -29,26 +31,35 @@ class CryptVault extends StatelessWidget {
             ListTile(
               leading: const Icon(Icons.safety_check),
               title: Column(
-                children:[
-                Text(accountController),
-                Text(passController),
+                children: [
+                  Text(accountController),
+                  Text(passController),
                 ],
               ),
               trailing: SizedBox(
-                width: 40,
+                width: 100,
                 child: Row(
                   children: [
-                    Expanded(child:IconButton(onPressed: (){
+                    Expanded(
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.edit),
+                      ),
+                    ),
+                    Expanded(
+                      child: IconButton(
+                        onPressed: () {
+                          final docVault = FirebaseFirestore.instance
+                              .collection('vault')
+                              .doc();
 
-
-                    }, icon: const Icon(Icons.edit))),
-                    Expanded(child:IconButton(onPressed: (){
-
-                        final docVault = FirebaseFirestore.instance.collection('vault').doc();
-
-                        docVault.delete();
-
-                    }, icon: const Icon(Icons.delete_outlined))),
+                          docVault.delete();
+                        },
+                        icon: const Icon(
+                          Icons.delete_outlined,
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
@@ -59,6 +70,3 @@ class CryptVault extends StatelessWidget {
     );
   }
 }
-
-
-
--- lib/dashscreen.dart
+++ lib/dashscreen.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'CryptVault.dart';
 
-
 void main() {
   runApp(const Dashscreen());
 }
@@ -10,15 +9,12 @@ void main() {
 class Dashscreen extends StatelessWidget {
   const Dashscreen({super.key});
 
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'CryptVault',
-      theme: ThemeData(
-
-      ),
+      theme: ThemeData(),
       home: const MyVault(),
     );
   }
@@ -30,48 +26,45 @@ class MyVault extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         centerTitle: true,
         title: const Text('CryptVault'),
         backgroundColor: Colors.blueAccent,
-
-    ),
-    backgroundColor: Colors.grey[850],
-    body: const Column(
-    crossAxisAlignment: CrossAxisAlignment.center,
-    children: [
-
-    Expanded(
-    child: AddButton(),
-    ),
-    ],
-    ),
+      ),
+      backgroundColor: Colors.grey[850],
+      body: const Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: AddButton(),
+          ),
+        ],
+      ),
     );
   }
 }
 
-class AddButton extends StatelessWidget{
+class AddButton extends StatelessWidget {
   const AddButton({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     final ButtonStyle style =
-    ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
 
     return Center(
-      child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            ElevatedButton(
-              style: style,
-              onPressed: (){
-                  Navigator.push(context,MaterialPageRoute(builder: (context)=> const MyCustomForm()),
-                  );
-              },
-              child: const Text('+'),
-            ),
-          ]
-      ),
+      child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+        ElevatedButton(
+          style: style,
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const MyCustomForm()),
+            );
+          },
+          child: const Text('+'),
+        ),
+      ]),
     );
   }
 }
@@ -85,13 +78,11 @@ class MyCustomForm extends StatefulWidget {
   }
 }
 
-
 class MyCustomFormState extends State<MyCustomForm> {
   // Global key that uniquely identifies the Form widget and allows validation of the form.
   final _formKey = GlobalKey<FormState>();
   final _accountController = TextEditingController();
   final _passController = TextEditingController();
-
 
   @override
   void dispose() {
@@ -102,47 +93,76 @@ class MyCustomFormState extends State<MyCustomForm> {
 
   @override
   Widget build(BuildContext context) {
-
     return AlertDialog(
       insetPadding: const EdgeInsets.symmetric(
-        horizontal: 50.0,
-        vertical: 100.0,
+        horizontal: 10,
+        vertical: 50.0,
       ),
       key: _formKey,
-      content: Column(
-        children: <Widget>[
-          TextField(
-            controller: _accountController,
-            decoration: const InputDecoration(
-              labelText: 'Account/Website name',
-              prefixIcon: Icon(Icons.account_circle_outlined),
-              border: OutlineInputBorder()
+      content: Container(
+        height: MediaQuery.of(context).size.height * 0.35,
+        width: MediaQuery.of(context).size.width * 0.9,
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 25.0),
+              child: Center(
+                child: Text(
+                  'Add New Credentials to Vault',
+                  style: TextStyle(
+                    color: Colors.blueAccent,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
             ),
-          ),
-          const SizedBox(height: 20,),
-          TextField(
-            controller: _passController,
-          decoration: const InputDecoration(
-          labelText: 'Password',
-           prefixIcon: Icon(Icons.security_sharp),
-            border: OutlineInputBorder()
-          ),
-         ),
-          const SizedBox(height: 20,),
-          OutlinedButton(
-              style: OutlinedButton.styleFrom(minimumSize: const Size(200,50)),
-              onPressed: (){
-                final vault = Vault(
-                  account: _accountController.text,
-                  password: _passController.text,
-                );
-                createVault(vault);
-                Navigator.push(context, MaterialPageRoute(builder: (context){
-                  return  CryptVault(accountController: _accountController.text, passController: _passController.text, );
-                }),
-                );
-              }, child: Text('Submit'.toUpperCase(),style: const TextStyle(color: Colors.blueAccent, fontWeight: FontWeight.bold),))
-        ],
+            TextField(
+              controller: _accountController,
+              decoration: const InputDecoration(
+                  labelText: 'Account/Website name',
+                  prefixIcon: Icon(Icons.account_circle_outlined),
+                  border: OutlineInputBorder()),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: _passController,
+              decoration: const InputDecoration(
+                  labelText: 'Password',
+                  prefixIcon: Icon(Icons.security_sharp),
+                  border: OutlineInputBorder()),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            OutlinedButton(
+                style:
+                    OutlinedButton.styleFrom(minimumSize: const Size(200, 50)),
+                onPressed: () {
+                  final vault = Vault(
+                    account: _accountController.text,
+                    password: _passController.text,
+                  );
+                  createVault(vault);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) {
+                      return CryptVault(
+                        accountController: _accountController.text,
+                        passController: _passController.text,
+                      );
+                    }),
+                  );
+                },
+                child: Text(
+                  'Add Credentials'.toUpperCase(),
+                  style: const TextStyle(
+                      color: Colors.blueAccent, fontWeight: FontWeight.bold),
+                ))
+          ],
+        ),
       ),
     );
   }
@@ -156,22 +176,20 @@ Future createVault(Vault vault) async {
   await docVault.set(json);
 }
 
-class Vault{
+class Vault {
   String id;
   final String account;
   final String password;
 
   Vault({
-   this.id= '',
-   required this.account,
-   required this.password,
-});
+    this.id = '',
+    required this.account,
+    required this.password,
+  });
 
   Map<String, dynamic> toJson() => {
-    'id': id,
-    'Account': account,
-    'Password':password,
-  };
+        'id': id,
+        'Account': account,
+        'Password': password,
+      };
 }
-
-
